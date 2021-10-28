744793fea89f24653e33d27b4c731c1561f923e1
Node Sans <node189@icloud.com>
2021-10-28 15:47:32 +0800

    增加了 jwt 的字段

--- utility/jwt.go
+++ utility/jwt.go
@@ -8,9 +8,10 @@ import (
 	"walk-server/utility/initial"
 )
 
-// 一些结构体的定义
-type jwtData struct {
-	OpenID string `json:"open_id"`
+// JwtData 一些结构体的定义
+type JwtData struct {
+	OpenID   string `json:"open_id"`
+	Identity string `json:"identity"`
 	jwt.StandardClaims
 }
 
@@ -19,8 +20,9 @@ func GenerateJWT(openID string) (string, error) {
 	nowTime := time.Now()
 	expireTime := nowTime.Add(3 * time.Hour)
 
-	claims := jwtData{
-		OpenID: fmt.Sprintf("%x", md5.Sum([]byte(openID))),
+	claims := JwtData{
+		OpenID:   fmt.Sprintf("%x", md5.Sum([]byte(openID))),
+		Identity: "not-join", // 刚注册完初始化身份为 not-join
 		StandardClaims: jwt.StandardClaims{
 			// 过期时间
 			ExpiresAt: expireTime.Unix(),
@@ -33,4 +35,22 @@ func GenerateJWT(openID string) (string, error) {
 	//该方法内部生成签名字符串，再用于获取完整、已签名的token
 	token, err := tokenClaims.SignedString([]byte(initial.Config.GetString("server.JWTSecret")))
 	return token, err
+}
+
+// ParseToken 根据传入的token值获取到Claims对象信息，（进而获取其中的用户名和密码）
+func ParseToken(token string) (*JwtData, error) {
+	jwtSecret := initial.Config.GetString("server.JWTSecret")
+	//用于解析鉴权的声明，方法内部主要是具体的解码和校验的过程，最终返回*Token
+	tokenClaims, err := jwt.ParseWithClaims(token, &JwtData{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		// 从tokenClaims中获取到Claims对象，并使用断言，将该对象转换为我们自己定义的Claims
+		// 要传入指针，项目中结构体都是用指针传递，节省空间。
+		if claims, ok := tokenClaims.Claims.(*JwtData); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+	return nil, err
 }

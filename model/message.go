785ba21eb9481570fdd51501365a49dd9ac55dd0
Node Sans <node189@icloud.com>
2022-03-02 11:07:25 +0800

    添加 message model 的一些方法

--- model/message.go
+++ model/message.go
@@ -1,8 +1,18 @@
 package model
 
+import "walk-server/global"
+
 type Message struct {
 	ID             uint
-	SenderOpenId   string
+	SenderOpenId   string // 如果发送者 open ID 为空, 相当于系统消息
 	ReceiverOpenId string `gorm:"index"`
 	Message        string
+}
+
+func InsertMessage(message string, senderOpenID string, receiverOpenID string) {
+	global.DB.Create(&Message{
+		SenderOpenId: senderOpenID,
+		ReceiverOpenId: receiverOpenID,
+		Message: message,
+	})
 }

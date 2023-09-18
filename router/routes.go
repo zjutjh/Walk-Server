5ee942a89b34b89ad1288bbffbff2f1b92bb163d
XiMo-210 <2831802697@qq.com>
2023-09-19 04:06:03 +0800

    路由部分修改

--- router/routes.go
+++ router/routes.go
@@ -1,4 +1,4 @@
-package main
+package router
 
 import (
 	"walk-server/controller/basic"
@@ -13,8 +13,12 @@ import (
 )
 
 func MountRoutes(router *gin.Engine) {
-	api := router.Group("/api/v1", middleware.TimeValidity)
+	api := router.Group("/api/v1")
 	{
+		if !gin.IsDebugging() {
+			api.Use(middleware.TimeValidity)
+		}
+
 		// Basic
 		api.GET("/oauth", basic.Oauth) // 微信 Oauth 的起点接口
 		api.GET("/login", basic.Login) // 微信服务器的回调地址
@@ -37,21 +41,21 @@ func MountRoutes(router *gin.Engine) {
 		teamApi := api.Group("/team", middleware.IsRegistered)
 		{
 			if gin.IsDebugging() {
-				teamApi.GET("/submit", team.SubmitTeam) // 提交团队
+				teamApi.GET("/submit", middleware.TokenRateLimiter, team.SubmitTeam) // 提交团队
 			} else {
-				teamApi.GET("/submit", middleware.IsExpired, middleware.CanSubmit, team.SubmitTeam) // 提交团队
+				teamApi.GET("/submit", middleware.TokenRateLimiter, middleware.IsExpired, middleware.CanSubmit, team.SubmitTeam) // 提交团队
 			}
 
-			teamApi.GET("/info", team.GetTeamInfo)                                              // 获取团队信息
-			teamApi.POST("/random-list", team.GetRandomList)                                    // 随机获取开放随机组队的团队列表
-			teamApi.POST("/random-join", middleware.IsExpired, team.RandomJoin)                 // 通过随机列表加入团队
-			teamApi.POST("/create", middleware.IsExpired, team.CreateTeam)                      // 创建团队
-			teamApi.POST("/update", middleware.IsExpired, team.UpdateTeam)                      // 修改队伍信息
-			teamApi.POST("/join", middleware.IsExpired, team.JoinTeam)                          // 加入团队
-			teamApi.GET("/leave", middleware.IsExpired, team.LeaveTeam)                         // 离开团队
-			teamApi.GET("/remove", middleware.IsExpired, team.RemoveMember)                     // 移除队员
-			teamApi.GET("/disband", middleware.IsExpired, team.DisbandTeam)                     // 解散团队
-			teamApi.GET("/rollback", middleware.IsExpired, team.RollBackTeam)                   // 撤销提交
+			teamApi.GET("/info", team.GetTeamInfo)                              // 获取团队信息
+			teamApi.POST("/random-list", team.GetRandomList)                    // 随机获取开放随机组队的团队列表
+			teamApi.POST("/random-join", middleware.IsExpired, team.RandomJoin) // 通过随机列表加入团队
+			teamApi.POST("/create", middleware.IsExpired, team.CreateTeam)      // 创建团队
+			teamApi.POST("/update", middleware.IsExpired, team.UpdateTeam)      // 修改队伍信息
+			teamApi.POST("/join", middleware.IsExpired, team.JoinTeam)          // 加入团队
+			teamApi.GET("/leave", middleware.IsExpired, team.LeaveTeam)         // 离开团队
+			teamApi.GET("/remove", middleware.IsExpired, team.RemoveMember)     // 移除队员
+			teamApi.GET("/disband", middleware.IsExpired, team.DisbandTeam)     // 解散团队
+			teamApi.GET("/rollback", middleware.IsExpired, team.RollBackTeam)   // 撤销提交
 		}
 
 		// 事件相关的 API

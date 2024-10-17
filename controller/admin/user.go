5b4a764bcf7818c5bf8a7990ceb3e18269341196
Penryn <15158052130@163.com>
2024-10-17 23:43:31 +0800

    fix(admin):修改更改个人状态为批量更改
    Signed-off-by: Penryn <15158052130@163.com>

--- controller/admin/user.go
+++ controller/admin/user.go
@@ -16,9 +16,13 @@ type UserStatusForm struct {
 	Status int    `json:"status" binding:"required,oneof=1 2"`
 }
 
+type UserStatusList struct {
+	List []UserStatusForm `json:"list" binding:"required"`
+}
+
 // UserSU 输入userID
 func UserStatus(c *gin.Context) {
-	var postForm UserStatusForm
+	var postForm UserStatusList
 	err := c.ShouldBindJSON(&postForm)
 	if err != nil {
 		utility.ResponseError(c, "参数错误")
@@ -26,34 +30,38 @@ func UserStatus(c *gin.Context) {
 	}
 
 	user, _ := adminService.GetAdminByJWT(c)
+	for _, form := range postForm.List {
+		// 获取个人信息
+		person, err := model.GetPerson(form.UserID)
+		if err != nil {
+			utility.ResponseError(c, "扫码错误，查找用户失败，请再次核对")
+			return
+		}
 
-	// 获取个人信息
-	person, err := model.GetPerson(postForm.UserID)
-	if err != nil {
-		utility.ResponseError(c, "扫码错误，查找用户失败，请再次核对")
-		return
-	}
+		var team model.Team
+		if err := global.DB.Where("id = ?", person.TeamId).Take(&team).Error; err != nil {
+			utility.ResponseError(c, "队伍信息获取失败")
+			return
+		}
 
-	var team model.Team
-	global.DB.Where("id = ?", person.TeamId).Take(&team)
-	// 管理员只能管理自己所在的校区
-	b := middleware.CheckRoute(user, &team)
-	if !b {
-		utility.ResponseError(c, "该队伍为其他路线")
-		return
-	}
+		// 管理员只能管理自己所在的校区
+		if !middleware.CheckRoute(user, &team) {
+			utility.ResponseError(c, "该队伍为其他路线")
+			return
+		}
 
-	if person.WalkStatus == 5 {
-		utility.ResponseError(c, "成员已结束毅行")
-		return
-	}
+		if person.WalkStatus == 5 {
+			utility.ResponseError(c, "成员已结束毅行")
+			return
+		}
 
-	if postForm.Status == 1 {
-		person.WalkStatus = 3
-	} else {
-		person.WalkStatus = 4
+		if form.Status == 1 {
+			person.WalkStatus = 3
+		} else {
+			person.WalkStatus = 4
+		}
+		userService.Update(*person)
 	}
-	userService.Update(*person)
 
 	utility.ResponseSuccess(c, nil)
 }

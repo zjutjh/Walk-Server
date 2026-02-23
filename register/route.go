989b7fec6feba88da7d8b2b18c36b69ebb5e0ea3
1917173927 <1917173927@qq.com>
2026-02-23 19:05:53 +0800

    fix:route分了user组

--- register/route.go
+++ register/route.go
@@ -19,25 +19,28 @@ func Route(router *gin.Engine) {
 	{
 		routeBase(r, router)
 
-		// 注册业务逻辑接口
-		r.GET("/wechat/login", api.WechatLoginHandler())
-
-		auth := r.Group("")
-		auth.Use(middleware.Auth())
+		user := r.Group("/user")
 		{
-			auth.POST("/register/student", api.RegisterStudentHandler())
-			auth.POST("/register/teacher", api.RegisterTeacherHandler())
-			auth.POST("/register/alumnus", api.RegisterAlumnusHandler())
+			// 注册用户端业务逻辑接口
+			user.GET("/wechat/login", api.WechatLoginHandler())
 
-			auth.GET("/user/info", api.UserInfoHandler())
-			auth.POST("/user/modify", api.UserModifyHandler())
+			auth := user.Group("")
+			auth.Use(middleware.Auth())
+			{
+				auth.POST("/register/student", api.RegisterStudentHandler())
+				auth.POST("/register/teacher", api.RegisterTeacherHandler())
+				auth.POST("/register/alumnus", api.RegisterAlumnusHandler())
 
-			auth.POST("/team/create", api.TeamCreateHandler())
-			auth.POST("/team/join", api.TeamJoinHandler())
-			auth.GET("/team/info", api.TeamInfoHandler())
-			auth.POST("/team/update", api.TeamUpdateHandler())
-			auth.GET("/team/leave", api.TeamLeaveHandler())
-			auth.GET("/team/disband", api.TeamDisbandHandler())
+				auth.GET("/info", api.UserInfoHandler())
+				auth.POST("/modify", api.UserModifyHandler())
+
+				auth.POST("/team/create", api.TeamCreateHandler())
+				auth.POST("/team/join", api.TeamJoinHandler())
+				auth.GET("/team/info", api.TeamInfoHandler())
+				auth.POST("/team/update", api.TeamUpdateHandler())
+				auth.GET("/team/leave", api.TeamLeaveHandler())
+				auth.GET("/team/disband", api.TeamDisbandHandler())
+			}
 		}
 
 	}
